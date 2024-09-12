7dfd605b9e6e1e9247097fd5137b8ea2c4683508
vitor <vitor.tavares.leite@gmail.com>
2024-09-12 11:55:30 -0300

    espero q funcione

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
+import App2 from './Home/Home';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
+      <Route path='/' element={<App2 />} />
+      <Route path='/frontendexpo' element={<App2 />} />
       <Route path="/frontendexpo/Search" element={<App />} />
 
     </Routes>
