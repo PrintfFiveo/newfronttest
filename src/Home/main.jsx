aabb875fe1a2737e1aa083988c5eae64ff50825d
vitor <vitor.tavares.leite@gmail.com>
2024-09-11 21:29:10 -0300

    kleber

--- src/Home/main.jsx
+++ src/Home/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './App.jsx'
+import Home from './Home.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
